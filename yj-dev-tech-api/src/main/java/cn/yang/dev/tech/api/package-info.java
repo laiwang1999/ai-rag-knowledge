93ca396547f8ab712800cbe0a83f0076aabb6b30
杨剑 <1422731368@qq.com>
2025-07-04 14:33:39 +0800

    feat: 第二节: 初始化知识库工程&提交代码

---
+++ yj-dev-tech-api/src/main/java/cn/yang/dev/tech/api/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @version v1.0
+ * @ClassName: cn.yang.dev.tech.api
+ * @Description: TODO
+ * @Author: YJ
+ */
+package cn.yang.dev.tech.api;
---
+++ yj-dev-tech-app/src/main/java/cn/yang/dev/tech/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @version v1.0
+ * @ClassName: cn.yang.dev.tech
+ * @Description: TODO
+ * @Author: YJ
+ */
+package cn.yang.dev.tech;
